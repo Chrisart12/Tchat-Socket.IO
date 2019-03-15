0ee624e316a56173d3c9a4248cda1a12b60e98ae
Chris <chrisissifou@gmail.com>
2019-03-15 20:48:40 +0100

    Modification suite aux téléchargements de bootstrap et md5

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 let http = require('http');
+let md5 = require('MD5');
+
 
 let server = http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.end('Salut tout le monde !');
+	res.end('Salut Issa !');
 });
 
-server.listen(8080);
+server.listen(1337);
+let io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function(socket) {
+
+	var me;
+	socket.on('login', function(user) {
+		me = user;
+		me.id = user.mail.replace('@','-').replace('.','-');
+		me.avatar = 'https://gravatar.com/avatar/' + md5(user.mail) + '?s=50';
+		socket.emit('logged');
+		io.sockets.emit('newusr', me);
+	})
+});
